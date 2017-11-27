65117b9c9bf20f3dc946e0226690bbdfcf7ab025
xiaogang00 <872310734@qq.com>
2017-11-27 13:20:19 +0800

    python code for chapter1

--- chapter1/chapter1.py
+++ chapter1/chapter1.py
@@ -29,3 +29,20 @@ plt.plot(x_test, y_test, c="g", label="$\sin(2\pi x)$")
 plt.legend()
 plt.show()
 
+for i, degree in enumerate([0, 1, 3, 9]):
+    plt.subplot(2, 2, i + 1)
+    feature = PolynomialFeatures(degree)
+    X_train = feature.transform(x_train)
+    X_test = feature.transform(x_test)
+
+    model = LinearRegressor()
+    model.fit(X_train, y_train)
+    y = model.predict(X_test)
+
+    plt.scatter(x_train, y_train, facecolor="none", edgecolor="b", s=50, label="training data")
+    plt.plot(x_test, y_test, c="g", label="$\sin(2\pi x)$")
+    plt.plot(x_test, y, c="r", label="fitting")
+    plt.ylim(-1.5, 1.5)
+    plt.annotate("M={}".format(degree), xy=(-0.15, 1))
+plt.legend(bbox_to_anchor=(1.05, 0.64), loc=2, borderaxespad=0.)
+plt.show()
