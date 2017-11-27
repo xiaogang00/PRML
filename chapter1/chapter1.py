f4248c65075749bd807c45f82541613717647fc9
xiaogang00 <872310734@qq.com>
2017-11-27 13:16:26 +0800

    python code for chapter1

--- chapter1/chapter1.py
+++ chapter1/chapter1.py
@@ -11,3 +11,21 @@ from prml.linear import (
 np.random.seed(1234)
 
 ## Example: polynomial curve fitting
+
+def create_toy_data(func, sample_size, std):
+    x = np.linspace(0, 1, sample_size)
+    t = func(x) + np.random.normal(scale=std, size=x.shape)
+    return x, t
+
+def func(x):
+    return np.sin(2 * np.pi * x)
+
+x_train, y_train = create_toy_data(func, 10, 0.25)
+x_test = np.linspace(0, 1, 100)
+y_test = func(x_test)
+
+plt.scatter(x_train, y_train, facecolor="none", edgecolor="b", s=50, label="training data")
+plt.plot(x_test, y_test, c="g", label="$\sin(2\pi x)$")
+plt.legend()
+plt.show()
+
